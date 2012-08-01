5b368ec851c5123d876a7cab8eae157e41f367ee
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2012-08-01 06:36:06 -0400

    fixing grub-install?

--- src/core/libs/lib-ui-interactive.sh
+++ src/core/libs/lib-ui-interactive.sh
@@ -1014,13 +1014,13 @@ EOF
 interactive_grubbios() {
 	debug 'FS' 'starting interactive_grubbios'
 
-	get_device_with_mount '/boot'
+	get_device_with_mount '/'
 	bootdev="$ANSWER_DEVICE"
 
 	modprobe dm-mod || die_error "Could not load dm-mod kernel module."
 
 	debug 'FS' "installing grub-bios to $bootdev"
-	chroot "$var_TARGET_DIR grub-install --target=i386-pc --recheck --debug $bootdev" || return 1
+	chroot "$var_TARGET_DIR" grub-install --target=i386-pc --recheck "$bootdev" || return 1
 
 	mkdir -p "$var_TARGET_DIR/boot/grub/locale"
 	cp "$var_TARGET_DIR/usr/share/locale/en/LC_MESSAGES/grub.mo" \
